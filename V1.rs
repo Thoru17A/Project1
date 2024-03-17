19debe577afe8c2e86735242209d0d54510d85fa
Thoru17A <163637659+Thoru17A@users.noreply.github.com>
2024-03-17 19:54:03 +0700

    Update V1.rs
    Добавление комментариев

--- V1.rs
+++ V1.rs
@@ -1,13 +1,17 @@
+// Импорт константы PI из модуля f64::consts
 use std::f64::consts::PI;
 
+// Функция для вычисления длины окружности по радиусу
 fn circle_length(radius: f64) -> f64 {
     2.0 * PI * radius
 }
 
+// Функция для вычисления площади окружности по радиусу
 fn circle_area(radius: f64) -> f64 {
     PI * f64::powf(radius, 2.0)
 }
 
+// Функция для вычисления площади сектора окружности по радиусу и углу
 fn sector_area(radius: f64, angle: f64) -> f64 {
     (angle / 360.0) * PI * f64::powf(radius, 2.0)
 }
