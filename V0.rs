7150808b4d35c434763f4d5cc4e8d7b105ada438
Thoru17A <163637659+Thoru17A@users.noreply.github.com>
2024-03-18 13:49:25 +0700

    Update V0.rs
    Исправлена ошибка с отрицательными числами.

--- V0.rs
+++ V0.rs
@@ -2,25 +2,43 @@
 use std::f64::consts::PI;
 
 // Функция для вычисления длины окружности по радиусу
-fn circle_length(radius: f64) -> f64 {
-    2.0 * PI * radius
+fn circle_length(radius: f64) -> Option<f64> {
+    if radius < 0.0 {
+        eprintln!("Ошибка: радиус не может быть отрицательным.");
+        return None;
+    }
+    Some(2.0 * PI * radius)
 }
 
 // Функция для вычисления площади окружности по радиусу
-fn circle_area(radius: f64) -> f64 {
-    PI * f64::powf(radius, 2.0)
+fn circle_area(radius: f64) -> Option<f64> {
+    if radius < 0.0 {
+        eprintln!("Ошибка: радиус не может быть отрицательным.");
+        return None;
+    }
+    Some(PI * f64::powf(radius, 2.0))
 }
 
 // Функция для вычисления площади сектора окружности по радиусу и углу
-fn sector_area(radius: f64, angle: f64) -> f64 {
-    (angle / 360.0) * PI * f64::powf(radius, 2.0)
+fn sector_area(radius: f64, angle: f64) -> Option<f64> {
+    if radius < 0.0 || angle < 0.0 {
+        eprintln!("Ошибка: радиус и угол не могут быть отрицательными.");
+        return None;
+    }
+    Some((angle / 360.0) * PI * f64::powf(radius, 2.0))
 }
 
 fn main() {
-    let radius: f64 = 5.0;
+    let radius: f64 = 25.0;
     let angle: f64 = 90.0;
-    
-    println!("Длина окружности: {}", circle_length(radius));
-    println!("Площадь окружности: {}", circle_area(radius));
-    println!("Площадь сектора окружности: {}", sector_area(radius, angle));
+
+    if let Some(length) = circle_length(radius) {
+        println!("Длина окружности: {}", length);
+    }
+    if let Some(area) = circle_area(radius) {
+        println!("Площадь окружности: {}", area);
+    }
+    if let Some(sector_area) = sector_area(radius, angle) {
+        println!("Площадь сектора окружности: {}", sector_area);
+    }
 }
